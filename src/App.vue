9c8c98cafc98e6a3e9b265d83d5631fecff5f8a4
wanghuan22 <wanghuan22@baidu.com>
2019-03-18 19:34:55 +0800

    start naming project

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-col :span="24">六十四卦</el-col>
+    </el-row>
+    <el-row>
+      <div>
+        ䷀ ䷁ ䷂ ䷃ ䷄ ䷅ ䷆ ䷇ ䷈ ䷉ ䷊ ䷋ ䷌ ䷍ ䷎ ䷏
+      </div>
+    </el-row>
+    <el-row>
+      <div>
+        ䷐ ䷑ ䷒ ䷓ ䷔ ䷕ ䷖ ䷗ ䷘ ䷙ ䷚ ䷛ ䷜ ䷝ ䷞ ䷟
+      </div>
+    </el-row>
+    <el-row>
+      <div>
+        ䷠ ䷡ ䷢ ䷣ ䷤ ䷥ ䷦ ䷧ ䷨ ䷩ ䷪ ䷫ ䷬ ䷭ ䷮ ䷯
+      </div>
+    </el-row>
+    <el-row>
+      <div>
+        ䷰ ䷱ ䷲ ䷳ ䷴ ䷵ ䷶ ䷷ ䷸ ䷹ ䷺ ䷻ ䷼ ䷽ ䷾ ䷿
+      </div>
+    </el-row>
+    <div>
+      <el-button @click="startHacking">Start</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'It works!',
+        type: 'success',
+        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+        duration: 5000
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
